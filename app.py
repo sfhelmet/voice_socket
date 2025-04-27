b30b32fdaf0f28eabd67822a0d0629ecbf7c4c35
Simon <30945652+sfhelmet@users.noreply.github.com>
2025-04-27 17:05:00 -0400

    remove evenlet

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
     import eventlet
     import eventlet.wsgi
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, server='eventlet')
+    socketio.run(app, host='0.0.0.0', port=port)

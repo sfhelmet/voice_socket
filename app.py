8eb7c1ea7eceebc6eb4a21d8fc31e3097a4ff0f4
Simon <30945652+sfhelmet@users.noreply.github.com>
2025-04-27 18:24:35 -0400

    working voice

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key')
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 @app.route('/')
 def index():
@@ -12,11 +12,8 @@ def index():
 
 @socketio.on('voice_data')
 def handle_voice_data(data):
-    # Forward the voice data to all other clients
-    emit('voice_data', data, broadcast=True, include_self=False)
+    emit('voice_data', data, broadcast=True, include_self=False, binary=True)
 
 if __name__ == '__main__':
-    import eventlet
-    import eventlet.wsgi
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
